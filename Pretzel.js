b0166cc410996713b08303b83957c2d9d6d0cb1a
Alberto <zeologism@hotmail.com>
2018-02-10 01:16:57 +0000

    update

--- Pretzel.js
+++ Pretzel.js
@@ -4,7 +4,7 @@
 
 const TeleBot = require('telebot');
 const request = require('request');
-const bot = new TeleBot('');
+const bot = new TeleBot('319752660:AAGQzKd1PdKr3R5ElVRz1ybMHtEA9oCg0mI');
 
 
 // Great API for this bot
