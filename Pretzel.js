7e229fefb87dfa12e1577d209c236243463f9a06
Alberto <zeologism@hotmail.com>
2018-04-14 12:58:12 +0000

    removed stickers

--- Pretzel.js
+++ Pretzel.js
@@ -4,14 +4,14 @@
 
 const TeleBot = require('telebot');
 const request = require('request');
-const bot = new TeleBot('');
+const bot = new TeleBot('319752660:AAGQzKd1PdKr3R5ElVRz1ybMHtEA9oCg0mI');
 
 
 // Great API for this bot
 const KITTYCAT = 'https://thecatapi.com/api/images/get?format=src&type=';
 const NASA = 'https://api.nasa.gov/planetary/apod?api_key=2rSHWsf3M3HOPf0qp3XHEzKaa5u47A1AB3peH9Ap';
 const CHUCK = 'https://api.chucknorris.io/jokes/random';
-const CRYPTO = 'https://www.bitstamp.net/api/v2/ticker/';
+const CRYPTO = 'https://www.bitstamp.net/api/v2/ticker/btceur/';
 
 
 // Command keyboard
@@ -33,9 +33,6 @@ bot.on('text', function(msg) {
     let YourName = msg.from.first_name;
     let Message = msg.text;
     
-    if (Message == 'dimme cojo uno') { 
-        return [
-           promise = bot.sendMessage(id, `mannaggia la madonna!`)]};
     if (Message == 'sei online?') { 
         return [
            promise = bot.sendMessage(id, `si ${ YourName }, sono online sul Raspberry Pi di Pi Dutz!`)]};
@@ -45,7 +42,7 @@ bot.on('text', function(msg) {
 bot.on(['/start', '/help'], function(msg) {
 
   return bot.sendMessage(msg.chat.id,
-     '😺 Use commands: /kitty, /kittygif, /space, /g (search gif), /chuck, /crypto', { markup }
+     '😺 Use commands: /kitty, /kittygif, /space, /g (BOOBS), /chuck, /crypto', { markup }
   );
 
 });
@@ -109,7 +106,7 @@ bot.on('/space', function(msg) {
 	});
 });
 
-// On command "g"
+// On command "g" Gif search
 bot.on('/g', function(msg) {
 
   let promise;
@@ -118,8 +115,8 @@ bot.on('/g', function(msg) {
   let clearspaces = text.replace(/\s/g,"+");
   let clearcommand = clearspaces.replace(/\/g/,"boobs");
   let search = 'search?q='+clearcommand;
-  console.log(search);
     let HOTTY = 'http://api.giphy.com/v1/gifs/'+search+'&limit=100&api_key=dc6zaTOxFJmzC&rating=r';
+    console.log(HOTTY);
     request(HOTTY, function (error, response, body) {
         if (!error && response.statusCode == 200) {
         GRRRL = JSON.parse(body);
@@ -176,88 +173,35 @@ bot.on(['/chuck', '/norris'], function(msg) {
 });
 
 // On command "Crypto"
-
 bot.on(['/crypto', '/coin'], function(msg) {
 
- var promise;
- var id = msg.chat.id;
- var text = msg.text;
- var coinsmb = text.replace(/\/crypto|\/coin/,"");      //remove the command from the inserted text (both /crypto and /coin)
- var nospaces = coinsmb.replace(/\s/g,"");              //remove any spaces
- var userInput = nospaces.split("-");                   //split user input using "-" as separator and store into an array
- var validPair01 = "btc bch eth ltc xrp";
- var validPair02 = "eur usd btc";
- var chk01 = new RegExp(userInput[0],"g");
- var chk02 = new RegExp(userInput[1],"g");
- var ticker = userInput[0].concat(userInput[1]);        //create a valid CoinPair string for Bitstamp API
-
-// Building header with coin name
-
-        if (userInput[0] == "btc") {
-         header = "Bitcoin";
-        }
-        else if (userInput[0] == "bch") {
-         header = "Bitcoin Cash";
-        }
-        else if (userInput[0] == "eth") {
-         header = "Ethereum";
-        }
-        else if (userInput[0] == "ltc") {
-         header = "Litecoin";
-        }
-        else if (userInput[0] == "xrp") {
-         header = "Ripple";
-        }
-
-// Check command and valid parameters
-
-        if (coinsmb  == "" || coinsmb == " help") {
-         return bot.sendMessage(id, `Usage: /crypto or /coin pair\n\nList of valid pairs:\nbtc-eur|usd\nbch-eur|usd|btc\neth-eur|usd|btc\nltc-eur|usd|btc\nxrp-eur|usd|btc\n\nEXAMPLE: /crypto btc-eur`);
-        }
-        else if  (userInput[0] == "btc" && userInput[1] == "btc") {
-         return bot.sendMessage(id, `Invalid request...Try again...`);
-        }
-        else if (validPair01.match(chk01) && validPair02.match(chk02)) {
-         ticker =  userInput[0].concat(userInput[1]);
-                if (userInput[1] == "eur"){
-                 smb = "EUR";
+  let promise;
+  let id = msg.chat.id;
+    
+  // Send "user is writing" action
+  bot.sendAction(id, 'typing');
+  
+	request(CRYPTO, function (error, response, body) {
+				if (!error && response.statusCode == 200) {
+					btceur = JSON.parse(body);
+           promise = bot.sendMessage(id, `1 BTC = ${ btceur.last }€, high ${ btceur.high }€, low ${ btceur.low }€` );
+				
                 }
-                else if (userInput[1] == "usd") {
-                 smb = "USD";
-                }
-                else if (userInput[1] == "btc") {
-                 smb = "BTC";
-                }
-        }
-        else {
-         return bot.sendMessage(id, `Invalid request...Try again...`);
-        }
-
-// Send "user is writing" action
-
- bot.sendAction(id, 'typing');
-
-// Getting data from exchange
-
- let CRYPTO = 'https://www.bitstamp.net/api/v2/ticker/'+ticker+'/';
-        request(CRYPTO, function (error, response, body) {
-                if (!error && response.statusCode == 200) {
-                 apiresp = JSON.parse(body);
-                 promise = bot.sendMessage(id, `${ header }\nLast price:  ${ apiresp.last } ${ smb }\nHigh price: ${ apiresp.high } ${ smb }\nLow price:  ${ apiresp.low } ${ smb }` );
-                }
-                else {
-                 console.log("offline.");
-                }
-        return promise.catch(error => {
-         console.log('[error]', error);
-// Send an error
-         bot.sendMessage(id, `An error ${ error } occurred, try again.`);
-         });
-        });
+				else {
+					console.log("offline.");
+				}
+        
+  return promise.catch(error => {
+    console.log('[error]', error);
+    // Send an error
+    bot.sendMessage(id, `😿 An error ${ error } occurred, try again.`);
+  });
+        
+	});
+    
 });
 
 // On command "kitty" or "kittygif"
-
 bot.on(['/kitty', '/kittygif'], function(msg) {
   
   let promise;
